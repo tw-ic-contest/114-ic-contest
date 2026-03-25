b02623c4e6f86a3ed7854a4bd76007eb65d534d4
Sheng-Yu-Cheng <sean.sheng.yu.cheng@gmail.com>
2026-03-25 10:02:25 +0800

    update tester

--- test/tb_test.sv
+++ test/tb_test.sv
@@ -25,19 +25,19 @@ module tb;
         $display("--------------------------------------------------");
 
         in1 = 32'd100; in2 = 32'd4;
-        #10;
+        #200;
         $display("[Time %0t] in1=%0d, in2=%0d | out = %0d (預期: 5)", $time, in1, in2, out);
 
         in1 = 32'd81; in2 = 32'd1;
-        #10;
+        #200;
         $display("[Time %0t] in1=%0d, in2=%0d | out = %0d (預期: 9)", $time, in1, in2, out);
 
         in1 = 32'd200; in2 = 32'd2;
-        #10;
+        #200;
         $display("[Time %0t] in1=%0d, in2=%0d | out = %0d (預期: 10)", $time, in1, in2, out);
 
         in1 = 32'd50; in2 = 32'd2;
-        #10;
+        #200;
         $display("[Time %0t] in1=%0d, in2=%0d | out = %0d (預期: 5)", $time, in1, in2, out);
 
         $display("--------------------------------------------------");
